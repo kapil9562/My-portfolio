4a69ae6521e94dadfdcc5f5ab7d62ed4b6c0c464
kapil9562 <adhikarikapil389@gmail.com>
2025-10-10 14:04:52 +0530

    Fix: added redirects for SPA routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
-import Layout from './Layout.jsx'
-import Home from './components/Home/Home.jsx'
-import About from './components/About/About.jsx'
-import Skills from './components/Skills/Skills.jsx'
-import Projects from './components/Projects/Projects.jsx'
-import Experiences from './components/Experiences/Experiences.jsx'
-import Contact from './components/Contact/Contact.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import Layout from './Layout.jsx';
+import Home from './components/Home/Home.jsx';
+import About from './components/About/About.jsx';
+import Skills from './components/Skills/Skills.jsx';
+import Projects from './components/Projects/Projects.jsx';
+import Experiences from './components/Experiences/Experiences.jsx';
+import Contact from './components/Contact/Contact.jsx';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import "@fortawesome/fontawesome-free/css/all.min.css";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<Layout/>} >
-      <Route index element={<Home/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/skills' element={<Skills/>}/>
-      <Route path='/projects' element={<Projects/>}/>
-      <Route path='/experiences' element={<Experiences/>}/>
-      <Route path='/contact' element={<Contact/>}/>
-    </Route>
-  )
-)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="skills" element={<Skills />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="experiences" element={<Experiences />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  </StrictMode>
+);
