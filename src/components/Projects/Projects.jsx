5d39787776fef73d1a38674ef2d2985aaf6502c6
kapil9562 <adhikarikapil389@gmail.com>
2026-08-28 19:31:08 +0530

    Refactor Projects component for improved card flipping animation and layout; enhance project display with image and explore button

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -75,106 +75,144 @@ function Projects() {
           viewport={{ once: true, amount: 0.4 }}
         >
 
-          {projects.map((project, idx) => (
+          {projects.map((project, idx) => {
 
-            <motion.div
-              variants={fadeIn}
-              key={idx}
-              viewport={{ once: true, amount: 0.4 }}
-              className={`w-full h-fit bg-transparent transform-style-preserve-3d cursor-pointer ${rotate[idx] ? "rotate-y-180" : "rotate-y-0"} transition-transform duration-700 relative transform-gpu will-change-transform group`}
-              onClick={() => openProject(project.live)}
-            >
+            return (
+              <motion.div
+                variants={fadeIn}
+                key={idx}
+                viewport={{ once: true, amount: 0.4 }}
+                className="w-full h-fit perspective-distant cursor-pointer bg-transparent group"
+              >
+                {/* 3D wrapper */}
+                <div
+                  className={`relative w-full h-fit transition-transform duration-700 ease-in-out transform-3d ${rotate[idx] ? "transform-[rotateY(180deg)]" : "transform-[rotateY(0deg)]"
+                    }`}
+                >
 
-              <div className={`bg-black cursor-pointer p-4 h-full w-full flex flex-col justify-between absolute top-0 left-0 z-10 rounded-2xl transition-all duration-500 -rotate-y-180 ${rotate[idx] ? "opacity-100 visible" : "opacity-0"} invisible`}>
+                  {/* BACK */}
+                  <div
+                    className={`absolute top-0 left-0 z-10 bg-black cursor-pointer h-full w-full flex flex-col justify-between rounded-2xl transition-all duration-300 backface-hidden transform-[rotateY(180deg)] ${rotate[idx] ? "opacity-100" : "opacity-0"
+                      }`}
+                    onClick={() => openProject(project.live)}
+                  >
+                    {/* rotating border */}
+                    <div className="card__border"></div>
 
-                <div className='space-y-5'>
-                  <div className='w-full flex flex-row justify-between items-start'>
-                    <FaRocket className='text-orange-700 text-4xl' />
+                    <div className='bg-black p-4 h-full w-full rounded-2xl flex flex-col justify-between'>
+                      <div className="space-y-5">
+                        <div className="w-full flex flex-row justify-between items-start">
+                          <FaRocket className="text-orange-700 text-4xl" />
+
+                          <button
+                            type="button"
+                            aria-label={`Flip ${project.title} card`}
+                            title={`Flip ${project.title} card`}
+                            className="text-gray-100 hover:text-orange-400 lg:group-hover:pointer-events-auto lg:pointer-events-none lg:group-hover:opacity-100 lg:opacity-0 transition-opacity duration-200 p-1 rounded-lg font-medium cursor-pointer absolute top-3 right-3 z-50"
+                            onClick={(e) => {
+                              e.stopPropagation();
+                              e.preventDefault();
+                              toggleRotate(idx);
+                            }}
+                          >
+                            <FaRotate className="text-lg hover:text-orange-400 transition-transform duration-500 active:rotate-180 " />
+                          </button>
+                        </div>
+
+                        <div className="card_title__container space-y-4">
+                          <h3 className="lg:text-3xl md:text-2xl sm:text-xl text-2xl font-bold bg-linear-to-r from-pink-500 to-orange-500 bg-clip-text text-transparent font-bricolage">
+                            {project.title}
+                          </h3>
+
+                          <div className="relative text-gray-100 text-sm">
+                            <p className="text-[#FFFFFF80] font-medium text-sm font-instrumentSans tracking-wide">
+                              {project.description}
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="flex flex-row flex-wrap gap-x-4 gap-y-1 text-sm">
+                        {project.tools.map((tool, i) => (
+                          <span
+                            className="text-orange-600 font-instrumentSans tracking-wide"
+                            key={i}
+                          >
+                            {tool}
+                          </span>
+                        ))}
+                      </div>
+
+                      <div className="absolute right-0 bottom-0 p-4">
+                        <span className="text-[#FFFFFF15] font-semibold text-8xl font-bricolage">
+                          {project.year}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+
+                  {/* FRONT */}
+                  <div
+                    className="card relative p-0! backface-hidden"
+                    onClick={() => openProject(project.live)}
+                  >
+
+                    {/* rotating border */}
+                    <div className="card__border"></div>
+
+                    {/* Flip button */}
                     <button
                       type="button"
                       aria-label={`Flip ${project.title} card`}
                       title={`Flip ${project.title} card`}
-                      className='text-gray-400 hover:text-orange-400 font-medium cursor-pointer'
+                      className="text-gray-100 hover:text-orange-400 bg-gray-800/60 lg:group-hover:pointer-events-auto lg:pointer-events-none lg:group-hover:opacity-100 lg:opacity-0 transition-opacity duration-200 p-1 rounded-lg font-medium cursor-pointer absolute top-3 right-3 z-50"
                       onClick={(e) => {
                         e.stopPropagation();
                         toggleRotate(idx);
-                      }}>
-                      <FaRotate className="text-gray-400 hover:text-orange-400 text-lg active:rotate-180 transition-all duration-500" />
+                      }}
+                    >
+                      <FaRotate className="text-lg active:rotate-180 transition-transform duration-500" />
                     </button>
-                  </div>
 
-                  <div className="card_title__container space-y-4">
-                    <h3 className="lg:text-3xl md:text-2xl sm:text-xl text-2xl font-bold bg-linear-to-r from-pink-500 to-orange-500 bg-clip-text text-transparent font-bricolage">{project.title}</h3>
-                    <div className="relative text-gray-100 text-sm">
+                    {/* image */}
+                    <div className="w-full overflow-hidden rounded-t-2xl">
+                      <img
+                        loading="eager"
+                        fetchPriority="high"
+                        decoding="async"
+                        src={project.image}
+                        alt="Thumbnail"
+                        className="h-auto w-full object-cover align-top group-hover:scale-110 transition-transform duration-2000"
+                      />
+                    </div>
 
-                      <p className={`text-[#FFFFFF80] font-medium text-sm font-instrumentSans tracking-wide`}>
-                        {project.description}
-                      </p>
+                    {/* content */}
+                    <div className="flex flex-col gap-4 p-4">
+                      <div className="card_title__container space-y-4">
+                        <h3 className="md:text-2xl text-xl font-bold text-gray-100 font-bricolage">
+                          {project.title}
+                        </h3>
+                      </div>
+
+                      <button
+                        type="button"
+                        aria-label={`Explore ${project.title}`}
+                        title={`Explore ${project.title}`}
+                        className="button border-2 border-pink-400 rounded-l-full rounded-r-full font-semibold flex flex-row justify-center items-center gap-4 font-instrumentSans"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          openProject(project.live);
+                        }}
+                      >
+                        <FaArrowUpRightFromSquare className='pointer-events-none' />
+                        <span className='pointer-events-none'>Explore</span>
+                      </button>
                     </div>
                   </div>
                 </div>
-
-                <div className='flex flex-row flex-wrap gap-x-4 gap-y-1 text-sm'>
-                  {project.tools.map((tool, i) => (
-                    <span className='text-orange-600 font-instrumentSans tracking-wide' key={i}>{tool}</span>
-                  ))}
-                </div>
-
-                <div className=' absolute right-0 bottom-0 p-4'>
-                  <span className='text-[#FFFFFF15] font-semibold text-8xl font-bricolage'>{project.year}</span>
-                </div>
-
-              </div>
-
-              <div
-                className={`card relative p-0!`}
-              >
-                <button
-                  type="button"
-                  aria-label={`Flip ${project.title} card`}
-                  title={`Flip ${project.title} card`}
-                  className='text-gray-100 hover:text-orange-400 bg-gray-800/60 lg:group-hover:pointer-events-auto lg:pointer-events-none lg:group-hover:opacity-100 lg:opacity-0 transition-opacity duration-200 p-1 rounded-lg font-medium cursor-pointer absolute top-3 right-3 z-10'
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    toggleRotate(idx);
-                  }}>
-                  <FaRotate className="text-lg active:rotate-180 transition-transform duration-500" />
-                </button>
-
-                {/* rotating border */}
-                <div className="card__border">
-                </div>
-
-                {/* {img} */}
-                <div className='w-full overflow-hidden rounded-t-2xl'>
-                  <img
-                    loading="eager"
-                    fetchPriority="high"
-                    decoding="async"
-                    src={project.image} alt="Thumbnail" className='h-auto w-full object-cover align-top group-hover:scale-110 transition-transform duration-2000' />
-                </div>
-
-                <div className='flex flex-col gap-4 p-4'>
-                  {/* content */}
-                  <div className="card_title__container space-y-4">
-                    <h3 className="md:text-2xl text-xl font-bold text-gray-100 font-bricolage">{project.title}</h3>
-                  </div>
-
-                  {/* button */}
-                  <button
-                    type="button"
-                    aria-label={`Explore ${project.title}`}
-                    title={`Explore ${project.title}`}
-                    className="button border-2 border-pink-400 rounded-l-full rounded-r-full font-semibold flex flex-row justify-center items-center gap-4 font-instrumentSans"
-                  >
-                    <FaArrowUpRightFromSquare />
-                    <span>Explore</span>
-                  </button>
-                </div>
-              </div>
-            </motion.div>
-
-          ))}
+              </motion.div>
+            );
+          })}
 
         </motion.div>
 
